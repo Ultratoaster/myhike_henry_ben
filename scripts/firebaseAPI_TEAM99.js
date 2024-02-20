cea2c85908f9ff287d6707dafb9f174c32e5d7d5
Ben Henry <bhenry18@my.bcit.ca>
2024-02-20 10:07:27 -0800

    Completed demo 7

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,18 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyC2UsVutJyVsGJ8JdXVNZVCQt_oKAEGhUY",
+
+    authDomain: "comp1800-demo-cdb90.firebaseapp.com",
+  
+    projectId: "comp1800-demo-cdb90",
+  
+    storageBucket: "comp1800-demo-cdb90.appspot.com",
+  
+    messagingSenderId: "340216776283",
+  
+    appId: "1:340216776283:web:f70de545e94b74357d617e"
+  
 };
 
 //--------------------------------------------
